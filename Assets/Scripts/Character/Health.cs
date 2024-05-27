fb7ce4673e5017610a958ef42cd1adb285ec7e18
David <jdiazga@unal.edu.co>
2024-05-27 10:57:13 -0500

    Add health to characters. Add visual effect when they are damaged

---
+++ Assets/Scripts/Character/Health.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float maxHealth;
+
+    float health;
+    bool isDead;
+    bool isInvincible;
+
+    public UnityEvent<float> OnAttacked;
+    public UnityEvent OnDead;
+
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    private void OnEnable()
+    {
+        Reset();
+    }
+
+    public float GetHealthPercentage()
+    {
+        return (health / maxHealth) * 100;
+    }
+    public float GetHealth()
+    {
+        return health;
+    }
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (isDead) return;
+        if (isInvincible) return;
+        health -= damage;
+        OnAttacked?.Invoke(damage);
+        if (health <= 0)
+        {
+            Die();
+            health = 0;
+        }
+    }
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void Reset()
+    {
+        health = maxHealth;
+        isDead = false;
+    }
+
+    public void SetInvincible(bool isInvincible)
+    {
+        this.isInvincible = isInvincible;
+    }
+
+    private void Die()
+    {
+        isDead = true;
+        OnDead?.Invoke();
+    }
+}
