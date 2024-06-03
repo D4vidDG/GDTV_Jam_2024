055fc3f5f2dd7ba27fc613c60f930feef1004bf3
David <jdiazga@unal.edu.co>
2024-06-03 09:59:23 -0500

    Add some randomness to enemies' path via PathModifier and varying the target

--- Assets/Scripts/Character/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Character/Enemy/EnemyMovement.cs
@@ -4,18 +4,72 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float targetVariance;
+    [SerializeField] float timeToRecalculateRandomTarget;
     AIPath AI;
-    AIDestinationSetter destinationSetter;
+
+    Vector2 currentDestination;
+    Transform desiredTarget;
+    Vector2 randomVector;
+    NNConstraint walkableNodesOnly;
+    float timer;
 
     private void Awake()
     {
         AI = GetComponent<AIPath>();
-        destinationSetter = GetComponent<AIDestinationSetter>();
     }
 
     private void Start()
     {
         AI.maxSpeed = speed;
+        RecalculateRandomTarget();
+        timer = 0;
+        walkableNodesOnly = new NNConstraint
+        {
+            constrainWalkability = true
+        };
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        float distanceToTarget = Vector2.Distance(desiredTarget.position, transform.position);
+        if (distanceToTarget < (targetVariance + 0.5f))
+        {
+            AI.destination = desiredTarget.position;
+        }
+        else
+        {
+            currentDestination = GetValidDestination();
+            AI.destination = currentDestination;
+        }
+
+        if (timeToRecalculateRandomTarget < timer)
+        {
+            RecalculateRandomTarget();
+            timer = 0;
+        }
+
+        Debug.DrawLine(transform.position, (Vector2)desiredTarget.position + randomVector);
+
+    }
+
+    private void RecalculateRandomTarget()
+    {
+        Debug.Log("Recalculating");
+        randomVector = GetRandomVector();
+    }
+
+    private Vector2 GetRandomVector()
+    {
+        //float randomVariance = Random.Range(0, targetVariance);
+        return Random.insideUnitCircle * targetVariance;
+    }
+
+    private Vector2 GetValidDestination()
+    {
+        Vector2 destination = (Vector2)desiredTarget.position + randomVector;
+        return (Vector2)(Vector3)AstarPath.active.GetNearest(destination, walkableNodesOnly).node.position;
     }
 
     public void Move()
@@ -31,11 +85,12 @@ public class EnemyMovement : MonoBehaviour
     public bool IsMoving()
     {
         return AI.canMove;
+
     }
 
     public void SetTarget(Transform target)
     {
-        destinationSetter.target = target;
+        desiredTarget = target;
     }
 
 }
